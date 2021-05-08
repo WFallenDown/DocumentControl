163c2d14779e804fc69bbd4f1ef2bca190a3b4d3
ALEX <wangbinim@outlook.com>
2021-05-08 12:39:47 +0800

    test channel

--- test/test.go
+++ test/test.go
@@ -6,19 +6,48 @@ import (
 	"io"
 	"math"
 	"os"
+	"path/filepath"
+	"strconv"
 )
 
 var number uint64
 
-func main() {
+var dir string
 
+var n int
+
+func main() {
+	var over string
 	fmt.Printf("test")
 	fmt.Println("\rtest2")
 
-	run()
+	dir = filepath.Dir(os.Args[0])
+	var c []chan int
+
+	for i := 0; i < 10; i++ {
+		c = append(c, make(chan int))
+		go runTest(i, c[i])
+	}
+
+	for _, index := range c {
+		d := <-index
+		fmt.Println(d)
+	}
+	fmt.Println(dir)
+	fmt.Scanln(&over)
 }
 
-func run() {
+func runTest(i int, c chan int) {
+	//time.Sleep(time.Duration(i) * time.Second)
+	n = i
+	fmt.Printf("--------------\n")
+	fmt.Println("n:", n, "i:", i)
+	fmt.Printf("..............\n")
+	c <- i
+	close(c)
+}
+
+func run(n int, c chan int) {
 	srcFile, err := os.Open("/Users/wangbin/OneDrive/Windows.iso")
 	if err != nil {
 		fmt.Printf("open file err=%v\n", err)
@@ -26,9 +55,8 @@ func run() {
 	defer srcFile.Close()
 	//通过src file ,获取到 Reader
 	reader := bufio.NewReader(srcFile)
-
 	//打开dstFileName
-	dstFile, err := os.OpenFile("./Windows.iso", os.O_RDWR|os.O_CREATE, 0666)
+	dstFile, err := os.OpenFile(dir+"/Windows"+strconv.Itoa(n)+".iso", os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Printf("open file err=%v\n", err)
 		return
@@ -47,6 +75,8 @@ func run() {
 	if err != nil {
 		panic(err)
 	}
+
+	c <- 1
 
 	// Prints output
 	fmt.Printf("n:%v\n", Reader)
@@ -68,14 +98,5 @@ func (wc *WriteCounter) PrintProgress() {
 	num := float64(wc.Total) / float64(number)
 	f := int(math.Floor((num * 100) + 0.5))
 
-	if wc.Item == 0.00 {
-		fmt.Printf("进度: \n")
-		wc.Item += 1
-	} else if wc.Item < 100 && f >= wc.Item {
-		fmt.Printf("\r %d %%", wc.Item)
-		wc.Item += 1
-	} else if f >= 100 {
-		fmt.Printf("\r 100%% \n")
-	}
-
+	fmt.Printf("\r %d %%", f)
 }
